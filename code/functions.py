40f2953cfec05c3e488394486c0e226dbb6deef3
Satoshi Ichikawa <sato41kawa0707@gmail.com>
2025-02-20 11:56:44 +0900

    update all

--- code/functions.py
+++ code/functions.py
@@ -7,7 +7,7 @@ warnings.simplefilter('ignore')
 
 def plot_z(year,z_mean,z_lower,z_upper,Y):
     plt.figure(figsize=(8, 4))
-    plt.plot(year, z_mean, label="Mean of z_t", color="blue", marker="o")
+    plt.plot(year, z_mean, label="Mean of z_t", color="blue")
     plt.fill_between(year, z_lower, z_upper, color="blue", alpha=0.2, label="95% Credible Interval")
     plt.title("Estimated Latent Variable z_t with 95% Credible Interval")
     plt.xlabel("Time")
